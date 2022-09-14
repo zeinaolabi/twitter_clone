3a554b3af54b08631b779702164280b75c9c8451
mariamhajir <mariam.hajir0@gmail.com>
2022-09-14 16:38:33 +0300

    Added Connection for the database.

---
+++ DatabaseConnection/connection.php
@@ -0,0 +1,22 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = null;
+$db_name = "twitter-clonedb";
+
+$mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+if ($mysqli->connect_errno) {
+    printf("connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+if ($mysqli->ping()) {
+    printf ("Our connection is ok!\n");
+} else {
+    printf("Error: %s\n", $mysqli->error);
+}
+
+?>
