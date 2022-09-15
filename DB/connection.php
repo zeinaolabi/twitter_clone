050942371e36bec0b90781d87be560740874c4f9
mariam <112480722+MariamHajir@users.noreply.github.com>
2022-09-15 18:15:39 +0300

    Fixed DB name.

--- DB/connection.php
+++ DB/connection.php
@@ -5,7 +5,7 @@ header("Access-Control-Allow-Origin: *");
 $db_host = "localhost";
 $db_user = "root";
 $db_pass = null;
-$db_name = "twitter-clonedb";
+$db_name = "twitterdb";
 
 $mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
 
