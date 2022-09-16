af41aa8e938123f9c5f0430053635e1a8671e42b
Zeina <zeina@Zeinas-MacBook-Pro.local>
2022-09-16 12:57:52 +0300

    Validated signin/signup input

--- js/login_script.js
+++ js/login_script.js
@@ -1,18 +1,52 @@
+//Initialize variables
+const signupAPI = "http://localhost/twitter_test/signup.php";
+const signinAPI = "http://localhost/twitter_test/signin.php";
 const signinModal = document.getElementById("signin_modal");
 const signupModal = document.getElementById("signup_modal");
-const signinButton = document.getElementById("signin");
-const signupButton = document.getElementById("signup");
+const signinModalButton = document.getElementById("signinbtn_modal");
+const signupModalButton = document.getElementById("signupbtn_modal");
 const createButton = document.getElementById("create_account");
 const closeSignin = document.getElementById("close_signin");
 const closeSignup = document.getElementById("close_signup");
+const userName = document.getElementById("name");
+const userEmail = document.getElementById("email");
+const userPass= document.getElementById("password");
+const loginButton = document.getElementById("login");
+const newUserName = document.getElementById("new_name");
+const newUserEmail = document.getElementById("new_email");
+const newUserPass= document.getElementById("new_password");
+const registerButton = document.getElementById("register");
 
-//When the user clicks on the button, open the modal
-signinButton.onclick = function() {
+//Adding drop down menus for date
+dates('option');
+months('option');
+years('option', 1990, 2022);
+
+//When the registration button is clicked, validate input 
+loginButton.addEventListener("click", (event)=>{
+    //If one of the functions return false, stop
+    if(!validateEmail(userEmail) || !validatePassword(userPassword)){
+        return
+    }
+
+    //Send the data to the database using POST method
+    fetch(signupAPI, {
+        method: 'POST',
+        body: new URLSearchParams({ "username": newUserName.value,
+        "email": newUserEmail.value,
+        "password": newUserPass.value}),
+    })
+    .then(response=>response.json())
+    .then(data => error.textContent = data.success)
+})
+
+//When the user clicks on the sign in button, open the modal
+signinModalButton.onclick = function() {
     signinModal.style.display = "block";
 }
 
-//When the user clicks on the button, open the modal
-signupButton.onclick = function() {
+//When the user clicks on the button sign up, open the modal
+signupModalButton.onclick = function() {
     signupModal.style.display = "block";
 }
 
@@ -22,17 +56,17 @@ createButton.onclick = function (){
     signupModal.style.display = "block";
 }
 
-//When the user clicks on x, close the modal
+//When the user clicks on x, close the modal(sign in)
 closeSignin.onclick = function() {
     signinModal.style.display = "none";
 }
 
-//When the user clicks on x, close the modal
+//When the user clicks on x, close the modal(sign up)
 closeSignup.onclick = function() {
     signupModal.style.display = "none";
 }
 
-//When the user clicks anywhere outside of the modal, close it
+//When the user clicks anywhere outside of the modals, close them
 window.onclick = function(event) {
     if (event.target == signinModal){
       signinModal.style.display = "none";
@@ -43,7 +77,42 @@ window.onclick = function(event) {
     }
 }
 
-//Adding drop down menus for date
-dates('option');
-months('option');
-years('option', 1990, 2022);
+function validateName(name){
+    //Check if the input is not empty and is a string
+    if(name.value == ""){
+        errorMessage = "Error: Missing field" 
+        return false
+    }
+    else if(typeof name.value !== "string"){
+        errorMessage = "Error: Invalid name";
+        return false
+    }
+
+    return true
+}
+
+function validateEmail(email){
+    //Check if the email is valid and has 3 characters after the @ and 5 after
+    if(email.value == ""){
+        errorMessage ="Error: Missing field" 
+        return false
+    }
+    else if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value) || 
+    email.value.substring(0,email.value.indexOf("@")).length < 3 ||
+    email.value.substring(email.value.indexOf("@") + 1,email.value.length).length < 5){
+        errorMessage = "Error: Invalid email";
+        return false
+    }
+    return true
+}
+
+function validateEmail(password){
+    //Check if the input is not empty and is a string
+    if(password.value == ""){
+        errorMessage = "Error: Missing field" 
+        return false
+    }
+
+    return true
+}
+
