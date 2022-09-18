51d61c7b400322f20db1a736d4837847bb400072
Zeina <zeina@Zeinas-MacBook-Pro.local>
2022-09-18 10:26:44 +0300

    Finished post tweet API (text only)

--- backend/postTweet.php
+++ backend/postTweet.php
@@ -17,4 +17,26 @@ if(!isset($tweet) || empty($tweet) || strlen($tweet) > 280){
     return;   
 }    
 
+$query = $mysqli->prepare("INSERT INTO `tweets` (`tweet`, `user_id`) VALUE (?, ?) "); 
+$query->bind_param("ss", $tweet, $userID);
+$query->execute();
+
+$tweet_id = mysqli_insert_id($mysqli);
+
+if ($tweet_id === null) {
+    http_response_code(400);
+    echo json_encode([
+        'error' => 400,
+        'message' => "Error: Can't add tweet"
+    ]);
+    
+    return;
+}
+
+$json = json_encode($array["status"] = "success!");
+echo $json;
+
+$query->close();
+$mysqli->close();
+
 ?>
