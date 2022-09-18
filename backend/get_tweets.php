8efdfdd1a80a19ec7735eae73ba2ebaec1b63deb
Zeina <zeina@Zeinas-MacBook-Pro.local>
2022-09-18 13:02:04 +0300

    Finished get tweets API

--- backend/get_tweets.php
+++ backend/get_tweets.php
@@ -15,7 +15,7 @@ if(!isset($userID) || empty($userID)){
     return;
 }
 
-$query = $mysqli->prepare("SELECT tweet, image
+$query = $mysqli->prepare("SELECT tweet, image, tweets.id
 FROM tweets
 LEFT JOIN images
 ON tweets.id = images.tweet_id
@@ -26,7 +26,6 @@ WHERE tweets.user_id IN (
     ON users.id = follows.user_id
     WHERE users.id = ?
 )");
-
 $query->bind_param("s", $userID);
 $query->execute();
 $array = $query->get_result();
@@ -41,13 +40,26 @@ if (empty($array)) {
     return;
 }
 
-$response = [];
-
+$tweets = [];
 while($a = $array->fetch_assoc()){
-    $response[] = $a;
+    $tweets[$a["id"]] = $a;
+    $tweets[$a["id"]]['likes_count'] = 0;
 }
 
-$json = json_encode($response);
+$query = sprintf("SELECT COUNT(*) as count, likes.tweet_id
+FROM likes
+where likes.tweet_id IN (%s)
+GROUP BY likes.tweet_id;", implode(',', array_keys($tweets)));
+
+$query = $mysqli->prepare($query);
+$query->execute();
+$array = $query->get_result();
+
+while($a = $array->fetch_assoc()){
+    $tweets[$a["tweet_id"]]['likes_counts'] = $a['count'];
+}
+
+$json = json_encode($tweets);
 echo $json;
 
 $query->close();
